e280635a86e6f147ec69e2781c6db8503c6e331c
darurie <santhosh.darurie@gmail.com>
2019-10-14 20:11:42 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,23 +2,33 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
+import 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      name: 'React'
+      name: 'Sanjo'
     };
   }
 
   render() {
     return (
+      <div class="topnav">
+  <a class="active" href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <input type="text" placeholder="Search.."/>
+
       <div>
         <Hello name={this.state.name} />
         <p>
-          magic happens :)
+          Belive and stay... magic happens :)
         </p>
       </div>
+</div>
+ 
+
     );
   }
 }
